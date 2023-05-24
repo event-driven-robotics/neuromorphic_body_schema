28ea8dd05cfdf7b54fc31105cd3ae365eb467d5d
Simon Mueller-Cleve <60852381+2103simon@users.noreply.github.com>
2023-05-24 11:50:40 +0200

    implementing contact info readout
    the taxel ID as well as the contact force are read and print to terminal

--- neuromorphic_body_schema/touch2e_mujoco.py
+++ neuromorphic_body_schema/touch2e_mujoco.py
@@ -10,26 +10,30 @@ import time
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def detect_contact(model, data):
+    ''' Loop to monitor contact information'''
     while True:
-        print("Gonna show a contact info soon my friend. Be patient!")
-        # for link in range(len(model.body_pos)):
-        # print(model.body_pos[mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_BODY, "r_hand_thumb_3")])
+        # print("Gonna show a contact info soon my friend. Be patient!")
+        # print(sensor_data)
+        sensor_data = data.sensordata
+        # only output info when non-zero contact detected
+        if np.sum(sensor_data) > 0.0:
+            taxel = np.where(sensor_data != 0.0)[0]  # get non-zero taxels IDs
+            values = sensor_data[taxel]  # get non-zero taxel readings
+            sensor_data = np.column_stack((taxel, values))  # here we fuse the taxel id and sensor reading in a 2d array
+            print("Sensor readings:")
+            print(sensor_data)
+            print('\n')
 
-        # base frame
-        # print(mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_BODY, "base_link"))
+# set model path
+model_path = './neuromorphic_body_schema/models/icub_v2_full_body_contact_sensors.xml'  # full iCub
+# model_path = 'neuromorphic_body_schema/models/icub_v2_right_hand_mk2_contact_sensor.xml'  # right hand only
+# DEBUG #
+# model_path = './models/icub_v2_full_body_contact_sensors.xml'  # full iCub
+# model_path = './models/icub_v2_right_hand_mk2_contact_sensor.xml'  # right hand only
 
-        # for geom1, geom2 in zip(data.contact.geom1, data.contact.geom2):
-        #     geom1_name = mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_BODY, geom1)
-        #     geom2_name = mujoco.mj_id2name(model, mujoco.mjtObj.mjOBJ_BODY, geom2)
-            
-        #     print(geom1_name, geom2_name)
-        print('\n\n')
-
-# model_path = 'neuromorphic_body_schema/models/icub_right_hand_position_actuators_actuate_hands_contact_sensor.xml'  # right hand only
-model_path = 'neuromorphic_body_schema/models/icub_position_actuators_actuate_hands_contact_sensors.xml'  # full iCub
 # Load the MuJoCo model and create a simulation
 model = mujoco.MjModel.from_xml_path(model_path)
 data = mujoco.MjData(model)
 
-# threading.Thread(target=detect_contact, args=(model, data, )).start()
+threading.Thread(target=detect_contact, args=(model, data)).start()
 viewer.launch(model, data)
